b84815d2e0a3c403f3bc1a08e7c7f465688e8e12
Kels Sepulveda Lopez <137461906+kelsllopez@users.noreply.github.com>
2025-05-07 14:52:44 -0400

    cambios

--- la_quiltra/settings.py
+++ la_quiltra/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY', default='dev-key')
 DEBUG = env.bool('DEBUG', default=True)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', 'la-casa-quiltra.onrender.com']
 
 
 
